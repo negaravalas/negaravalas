9eee2b07b0434eee5a9796c19e9022313ce9e9b9
negaravalas <negaravalas@gmail.com>
2025-11-02 14:30:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const currencyData = [
   { currency: "CHF", rate: "-", flag: "https://flagcdn.com/w320/ch.png" },
   { currency: "SGD", rate: "-", flag: "https://flagcdn.com/w320/sg.png" },
   { currency: "MYR", rate: "-", flag: "https://flagcdn.com/w320/my.png" },
-  { currency: "MYR K", rate: "-", flag: "https://flagcdn.com/w320/my.png" },
+  { currency: "MYR T", rate: "-", flag: "https://flagcdn.com/w320/my.png" },
   { currency: "HKD", rate: "-", flag: "https://flagcdn.com/w320/hk.png" },
   { currency: "NTY", rate: "-", flag: "https://flagcdn.com/w320/tw.png" },
   { currency: "KRW", rate: "-", flag: "https://flagcdn.com/w320/kr.png" },
@@ -373,5 +373,6 @@ if (typeof module !== 'undefined' && module.exports) {
         initializeFirebase
     };
 }
+
 
 
